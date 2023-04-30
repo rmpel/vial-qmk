05d02c403c3e478edcb3c09a757b21fcbd3d784a
Remon Pel <ik@remonpel.nl>
2023-04-30 18:02:40 +0200

    add mouse layer

--- keyboards/remonpel/rp_numpad_4x6/keymaps/via/keymap.c
+++ keyboards/remonpel/rp_numpad_4x6/keymaps/via/keymap.c
@@ -7,21 +7,21 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┐
-     * │M1 │M2 │M3 │M4 │
+     * │M1 │M2 │MSE│SLP│
      * ├───┼───┼───┼───┤
      * │Num│ / │ * │ - │
      * ├───┼───┼───┼───┤
      * │ 7 │ 8 │ 9 │   │
-     * ├───┼───┼───┤ + |
+     * ├───┼───┼───┤ + │
      * │ 4 │ 5 │ 6 │   │
      * ├───┼───┼───┼───┤
      * │ 1 │ 2 │ 3 │   │
-     * ├───┴───┼───┤Ent|
+     * ├───┴───┼───┤Ent│
      * │   0   │ . │   │
      * └───────┴───┴───┘
      */
     [0] = LAYOUT_ortho_6x4(
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   TG(2),   KC_SLEP,
         KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         KC_P7,   KC_P8,   KC_P9,
         KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Num│ / │ * │ - │
      * ┌───┬───┬───┐───┤
      * │Hom│ ↑ │PgU│   │
-     * ├───┼───┼───┤ + |
+     * ├───┼───┼───┤ + │
      * │ ← │   │ → │   │
      * ├───┼───┼───┤───┤
      * │End│ ↓ │PgD│   │
-     * ├───┴───┼───┤Ent|
+     * ├───┴───┼───┤Ent│
      * │  Ins  │Del│   │
      * └───────┴───┘───┘
      */
@@ -51,6 +51,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LEFT, XXXXXXX, KC_RGHT, _______,
         KC_END,  KC_DOWN, KC_PGDN,
         KC_INS,           KC_DEL,  _______
+    ),
+
+
+    /*
+     * ┌───┬───┬───┬───┐
+     * │   │   │MSE│   │
+     * ├───┼───┼───┼───┤
+     * │   │Lmb│Mmb│Rmb│
+     * ├───┼───┼───┼───┤
+     * │   │ ↑ │   │ w │
+     * ├───┼───┼───┤ ↑ │
+     * │ ← │   │ → │   │
+     * ├───┼───┼───┼───┤
+     * │   │ ↓ │   │ w │
+     * ├───┴───┼───┤ ↓ │
+     * │   >   │>>>│   │
+     * └───────┴───┴───┘
+     */
+    [2] = LAYOUT_ortho_6x4(
+        XXXXXXX, XXXXXXX, TG(2),   _______,
+        XXXXXXX, KC_BTN1, KC_BTN3, KC_BTN2,
+        XXXXXXX, KC_MS_U, XXXXXXX,
+        KC_MS_L, XXXXXXX, KC_MS_R, KC_WH_U,
+        XXXXXXX, KC_MS_D, XXXXXXX,
+        KC_ACL0,          KC_ACL2, KC_WH_D
     )
 };
 
